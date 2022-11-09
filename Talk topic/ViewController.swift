647beb6d91bf313024d450b9779fe1d68a56cdd4
AlenaBelenets <alenaharenkova@gmail.com>
2022-11-09 15:26:10 +0300

    Initial Commit

---
+++ Talk topic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Talk topic
+//
+//  Created by Alena Belenets on 09.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
